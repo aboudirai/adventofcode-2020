5a2fab6a53812c312ac045e7751edcc010e86184
aboudirai <raiaboudi@yahoo.com>
2020-12-23 20:44:47 -0500

    first part

--- day8/d8.py
+++ day8/d8.py
@@ -2,13 +2,59 @@ inst = []
 
 with open("input.txt") as file:
     for line in file.readlines():
-        inst.append(line)
+        temp = line.split(' ')
+        val = temp[1]
+        if val[0] == '-':
+            val = int(val[1:]) * -1
+        else:
+            val = int(val[1:])
 
-print(inst)
+        inst.append([temp[0], val])
 
+#-----------------
 #part 1
 
+acc = 0
+pc = 0
+visited = []
+
+while pc not in visited:
+    visited.append(pc)
+    op = inst[pc][0]
+    val = inst[pc][1]
+    if op == 'acc':
+        acc += val
+        pc += 1
+    elif op == 'jmp':
+        pc += val
+    else:
+        pc += 1
+
+print(acc)
 
 #------------
-
 #part 2
+
+acc = 0
+pc = 0
+visited = []
+insts = []
+jmpnnop = 0
+while pc not in visited:
+    visited.append(pc)
+    insts.append(inst)
+    op = inst[pc][0]
+    val = inst[pc][1]
+    if op == 'acc':
+        acc += val
+        pc += 1
+    elif op == 'jmp':
+        pc += val
+        jmpnnop += 1
+    else:
+        pc += 1
+        jmpnnop += 1
+
+
+print(jmpnnop)
+print(len(insts))
